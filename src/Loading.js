4cdf9a9fb94a39f012b545e9a1c57d4339d8288f
Umesh Raghav <umeshraghav55@gmail.com>
2021-08-04 13:17:28 +0530

    CSS for loading component

--- src/Loading.js
+++ src/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <section className="section">
+        <section className="loading">
                 <h1>Loading...</h1>
         </section>
     )
